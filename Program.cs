9380ff90ef9d1e19cbc6e51e7a72341d99e77e6d
Renato <renato.lage@ligohub.com>
2023-11-17 21:22:02 -0300

    Implem. modulos VerificaPosicao e PreenchePosicao

--- Program.cs
+++ Program.cs
@@ -1,11 +1,24 @@
 ﻿using System;
 using System.Globalization;
+using System.IO.Compression;
 class Program
 {
+    static char[,] PreencherTabuleiroVazio(char[,] tabuleiro)
+    { 
+        for(int i = 0; i < tabuleiro.GetLength(0); i++)
+        {
+            for(int j = 0; j < tabuleiro.GetLength(1); j++)
+            {
+                tabuleiro[i,j] = 'A';
+            }
+        }
+        return tabuleiro;
+    }
     static void ImprimirTabuleiro(char[,] tabuleiro)
     {   Console.Write("   0 1 2 3 4 5 6 7 8 9");
         Console.WriteLine();
         for(int i = 0; i < tabuleiro.GetLength(0); i++)
+
         {   Console.Write(i + " ");
             for(int j = 0; j < tabuleiro.GetLength(1); j++)
             {
@@ -14,32 +27,72 @@ class Program
             Console.WriteLine("|");
         }
     }
-    static char[,] PreencheTabuleiroHumano ()
-    {
-        char[,] tabuleiroHumano = new char[10,10];
-        int[] embarcacoes = {4,3,2,1,1};
-        int escolha;
-        Console.WriteLine("Digite o tipo de embarcação que deseja posicionar:");
-        Console.WriteLine("1 - Submarino");
-        Console.WriteLine("2 - Hidroavião");
-        Console.WriteLine("3 - Cruzador");
-        Console.WriteLine("4 - Encouraçado");
-        Console.WriteLine("5 - Porta-Aviões");
-        escolha = int.Parse(Console.ReadLine());
-        switch (escolha)
-        {
-            case 1:
-                Console.WriteLine("Escolha a posição no tabuleiro abaixo:");            
-            break;
-        }
-        
 
-        return tabuleiroHumano;
+    static char[,] PreenchePosicao(char[,] tabuleiro, int coordX, int coordY, int tipoNavio, char posicaoNavio)
+    {
+        char[,] tabuleiroPreenchido = tabuleiro;
+        char[] sigla = {'S','H','C','E','P'};
+        if(posicaoNavio == 'V')
+        {
+            for(int i = 0; i < tipoNavio; i++)
+            {
+                tabuleiroPreenchido[coordX, coordY+i] = sigla[tipoNavio-1];
+            }
+        }
+        else if(posicaoNavio == 'H')
+        {
+            for(int i = 0; i < tipoNavio; i++)
+            {
+                tabuleiroPreenchido[coordX+i,coordY] = sigla[tipoNavio-1];
+            }
+        }
+        return tabuleiroPreenchido;
+
     }
+    static bool VerificaPosicao(char[,] tabuleiro, int coordX, int coordY, int tipoNavio, char posicaoNavio)
+    {
+        if((posicaoNavio == 'V' && (tipoNavio+coordY) > 10) || (posicaoNavio == 'H' && (tipoNavio+coordX) > 10))
+        {
+            Console.WriteLine("Navio fora do tabuleiro.");
+            return false;
+        }
+        else if(posicaoNavio == 'V')
+        {
+            for (int i = 0; i < tipoNavio; i++)
+            {
+                if(tabuleiro[coordX,coordY+i] != 'A')
+                {
+                    Console.WriteLine("Já existe um navio nessa coordenada.");
+                    return false;
+                }
+            }
+        }
+        else if(posicaoNavio == 'H')
+        {
+            for(int i = 0; i < tipoNavio; i++)
+            {
+                if(tabuleiro[coordX+i,coordY] != 'A')
+                {
+                    Console.WriteLine("Já existe um navio nessa coordenada.");
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
     static void Main()
     {
         char[,] teste = new char[10,10];
-        ImprimirTabuleiro(PreencheTabuleiroHumano());
+        teste = PreencherTabuleiroVazio(teste);
+        ImprimirTabuleiro(teste);
+        PreenchePosicao(teste,5,5,3,'V');
+        ImprimirTabuleiro(teste);
+        PreenchePosicao(teste,0,0,5,'V');
+        ImprimirTabuleiro(teste);
+        PreenchePosicao(teste,2,0,4,'H');
+        ImprimirTabuleiro(teste);
     }
 }
 
